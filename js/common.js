9a86093bb4173d49c1627615a74366acd2a5c0e7
songxx <913581148@qq.com>
2017-06-27 14:14:49 +0800

    amending

--- js/common.js
+++ js/common.js
@@ -53,3 +53,40 @@ function pageScrollTop() {
 function pageScrollBottom() {
     document.body.scrollTop=document.body.scrollHeight;
 }
+
+function getExplorerInfo() {
+	var explorer = window.navigator.userAgent.toLowerCase() ;
+	//ie 
+	if (explorer.indexOf("msie") >= 0) {
+		var ver=explorer.match(/msie ([\d.]+)/)[1];
+		return {type:"IE",version:ver};
+	}
+	//firefox 
+	else if (explorer.indexOf("firefox") >= 0) {
+		var ver=explorer.match(/firefox\/([\d.]+)/)[1];
+		return {type:"Firefox",version:ver};
+	}
+	//Chrome
+	else if(explorer.indexOf("chrome") >= 0){
+		var ver=explorer.match(/chrome\/([\d.]+)/)[1];
+		return {type:"Chrome",version:ver};
+	}
+	//Opera
+	else if(explorer.indexOf("opera") >= 0){
+		var ver=explorer.match(/opera.([\d.]+)/)[1];
+		return {type:"Opera",version:ver};
+	}
+	//Safari
+	else if(explorer.indexOf("Safari") >= 0){
+		var ver=explorer.match(/version\/([\d.]+)/)[1];
+		return {type:"Safari",version:ver};
+	}
+}
+ var types = getExplorerInfo().type;
+ var version = getExplorerInfo().version;
+ if(types === 'IE') {
+ 	if(version === '8.0' || version === '7.0' || version === '6.0') {
+ 		alert("IE浏览器版本过低，请到指定网站去下载相关版本"); 
+ 		window.open("http://outdatedbrowser.com/zh-cn");  
+ 	}
+ }
